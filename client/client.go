7abdf994e123363cfc6281708fd55c753e0a9dc2
Eva <evaf@itu.dk>
2023-11-26 23:38:24 +0100

    refactoring and resultMethod for auctionNodes

--- client/client.go
+++ client/client.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
-
 	//proto "Handin5Auction/protoFile"
-
 	//"google.golang.org/grpc"
 )
 
@@ -13,19 +11,19 @@ type AuctionNode struct {
 	id            int
 	highestBid    int
 	highestBidder string
-	bidders       map[string]struct{
-		amount int64
+	bidders       map[string]struct {
+		amount  int64
 		lamTime int64
 	}
-	mu            sync.Mutex
-	result        string
+	mu      sync.Mutex
+	result  string
 	lamTime int64
 }
 
 type BidRequest struct {
 	bidderID string
 	amount   int64
-	lamTime int64
+	lamTime  int64
 }
 
 type bidResult int64
@@ -44,13 +42,12 @@ type ResultRequest struct {
 	bidderID string
 }
 
-
 type Outcome int64
 
 const (
 	AUCTION_NOT_OVER Outcome = iota
-    AUCTION_SUCCESS
-    AUCTION_FAIL
+	AUCTION_SUCCESS
+	AUCTION_FAIL
 )
 
 type ResultResponse struct {
@@ -65,7 +62,10 @@ func (n *AuctionNode) Bid(request *BidRequest) *BidResponse {
 
 	// Check if the bidder is registered
 	if _, exists := n.bidders[request.bidderID]; !exists {
-		n.bidders[request.bidderID] = struct{amount int64; lamTime int64}{0,0}
+		n.bidders[request.bidderID] = struct {
+			amount  int64
+			lamTime int64
+		}{0, 0}
 	}
 
 	// Check if the bid is higher than the previous one
@@ -74,19 +74,39 @@ func (n *AuctionNode) Bid(request *BidRequest) *BidResponse {
 	}
 
 	// Update the bid
-	n.bidders[request.bidderID] = struct{amount int64; lamTime int64}{request.amount, request.lamTime}
+	n.bidders[request.bidderID] = struct {
+		amount  int64
+		lamTime int64
+	}{request.amount, request.lamTime}
 
 	n.lamTime = max(n.lamTime, request.lamTime) + 1
 	return &BidResponse{bidResult: BidSuccess}
 }
 
+func (n *AuctionNode) ResultMethod() Outcome {
+	resultResponse := n.result
+
+	switch resultResponse {
+	case "AUCTION_NOT_OVER":
+		fmt.Printf("Auction is still ongoing and the highest bid is %d", n.highestBid)
+		return AUCTION_NOT_OVER
+	case "AUCTION_SUCCESS":
+		fmt.Printf("Auction won by %s with a bid of %d\n", n.highestBidder, n.highestBid)
+		return AUCTION_SUCCESS
+	case "AUCTION_FAIL":
+		fmt.Println("Auction failed")
+		return AUCTION_FAIL
+	}
+}
+
 func main() {
 	node := &AuctionNode{
-		id:      1,
-		bidders: make(map[string]struct{
-			amount int64
+		id: 1,
+		bidders: make(map[string]struct {
+			amount  int64
 			lamTime int64
 		}),
+		result: "AUCTION_NOT_OVER",
 	}
 
 	//some bids:
@@ -98,14 +118,5 @@ func main() {
 	node.Bid(bidRequest2)
 	node.Bid(bidRequest3)
 
-	resultResponse := node.result
-
-	switch resultResponse {
-	case "AUCTION_NOT_OVER":
-		fmt.Println("Auction is still ongoing")
-	case "AUCTION_SUCCESS":
-		fmt.Printf("Auction won by %s with a bid of %d\n", node.highestBidder, node.highestBid)
-	case "AUCTION_FAIL":
-		fmt.Println("Auction failed")
-	}
+	node.ResultMethod()
 }
